f4b0e47bb5df007c0911904f298b377594558894
maneeshavedicherla6 <78723973+maneeshavedicherla6@users.noreply.github.com>
2024-08-22 22:16:02 +0530

    Add files via upload

---
+++ Patterns/P5.java
@@ -0,0 +1,21 @@
+public class P5 {
+
+	public static void main(String[] args) {
+		int count=1;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				if(count<10)
+				{
+					System.out.print("0");
+				}
+				System.out.print(count+" ");
+				count++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
