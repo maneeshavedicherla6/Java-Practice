902be07653e45b7d744de78bedcebd1787ef00cf
maneeshavedicherla6 <78723973+maneeshavedicherla6@users.noreply.github.com>
2024-08-23 21:23:53 +0530

    Add files via upload

---
+++ Patterns/P8.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class P8 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=scn.nextInt();
+		int space=5;
+		int star=1;
+		for(int i=1;i<=n;i++)
+		{
+			for(int k=1;k<=space;k++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				if(j==1||j==i||i==n) 
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+				
+			}
+			System.out.println();
+			star++;
+			space--;
+		}
+	}
+
+}
