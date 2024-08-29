8edea47d166e84635ace6a0901a89bbe452d6b9c
maneeshavedicherla6 <78723973+maneeshavedicherla6@users.noreply.github.com>
2024-08-29 23:45:35 +0530

    Add files via upload

---
+++ Number Programs/Prev.java
@@ -0,0 +1,40 @@
+//write a program to print previous palindrome number from the given number
+
+import java.util.Scanner;
+class Prev
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter the number: ");
+		int start=scn.nextInt();
+		for(int i=start;i>=i-1;i--)
+		{
+			if(ispal(i))
+			{
+				System.out.println(i);
+				break;
+			}
+		
+		}
+	}
+	public static boolean ispal(int n)
+	{
+		int num=n;
+		int rev=0;
+		while(num>0)
+		{
+			int d=num%10;
+			rev=rev*10+d;
+			num/=10;
+		}
+		if(n==rev){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
+		
