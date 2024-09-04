9232fec039743a3f4cb0d7836ca65943dc73dded
maneeshavedicherla6 <78723973+maneeshavedicherla6@users.noreply.github.com>
2024-09-04 20:26:06 +0530

    Add files via upload

---
+++ Arrays/MergeArrays.java
@@ -0,0 +1,30 @@
+public class MergeArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+        int[] mergedArray = new int[arr1.length + arr2.length];
+        
+        int i = 0, j = 0, k = 0;
+        
+        while(i < arr1.length && j < arr2.length) {
+            if(arr1[i] < arr2[j]) {
+                mergedArray[k++] = arr1[i++];
+            } else {
+                mergedArray[k++] = arr2[j++];
+            }
+        }
+        
+        while(i < arr1.length) {
+            mergedArray[k++] = arr1[i++];
+        }
+        
+        while(j < arr2.length) {
+            mergedArray[k++] = arr2[j++];
+        }
+        
+        System.out.println("Merged array: ");
+        for(int x = 0; x < mergedArray.length; x++) {
+            System.out.print(mergedArray[x] + " ");
+        }
+    }
+}
