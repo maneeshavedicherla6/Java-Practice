fc05173f2b6e78f7a9eef4de60cbb9406fab0647
maneeshavedicherla6 <78723973+maneeshavedicherla6@users.noreply.github.com>
2024-09-06 21:32:25 +0530

    Add files via upload

---
+++ Arrays/TransposeMatrix.java
@@ -0,0 +1,36 @@
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        // Define a 3x3 matrix
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original matrix:");
+        printMatrix(matrix);
+        
+        System.out.println("Transpose of the matrix:");
+        transposeMatrix(matrix);
+    }
+
+    // Method to transpose and print a matrix
+    public static void transposeMatrix(int[][] matrix) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Method to print a matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
